76aa8f76607f473d35cdc3a9a0484b894781528a
Kavia AI <production@kavia.ai>
2026-02-19 14:02:27 +0000

    Checkpoint 1 - cg3014d842

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/career-profile-builder-42831-42724/frontend_nextjs
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
